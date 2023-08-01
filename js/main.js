4f8e1ce016f27b1eab0abf24862588cdfa5953c4
sunchoi22 <sunchoi22@pos.idserve.net>
2023-08-01 15:21:50 +0900

    [ADD & FEAT] header icon 추가 및 main_header html 추가

---
+++ js/main.js
@@ -0,0 +1,8 @@
+let toggleBtn = $('.main_header_toggleBtn'),
+    menu = $('.main_header_menu'),
+    icons = $('.main_header_icons');
+
+    toggleBtn.on('click',function(e){
+      e.preventDefault();
+      
+    })   
