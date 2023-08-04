70f1a9de463ff2fb2580030e66bf3d3e4060fd99
sunchoi22 <sunchoi22@pos.idserve.net>
2023-08-04 18:05:03 +0900

    [FIX] header 반응형 포함 수정중입니다.

--- js/main.js
+++ js/main.js
@@ -1,8 +1,11 @@
-let toggleBtn = $('.main_header_toggleBtn'),
-    menu = $('.main_header_menu'),
-    icons = $('.main_header_icons');
+$('.main_header_menu > li').click(function() {
+  $('.main_header_menu > li').not(this).find('ul').slideUp(300);
+  $(this).find('ul').stop().slideToggle(300);  
+});
 
-    toggleBtn.on('click',function(e){
-      e.preventDefault();
-      
-    })   
+// .main_header_toggleBtn을 클릭하면 .main_header_menu가 보이거나 숨겨집니다.
+$('.main_header_toggleBtn').click(function() {
+  $('.main_header_menu').toggle();
+});
+
+  $( "#accordion" ).accordion();
