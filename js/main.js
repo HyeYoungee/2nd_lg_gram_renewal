52767b2876590adc42d0385e0dc4937d50cf907e
sunchoi22 <sunchoi22@pos.idserve.net>
2023-08-06 02:14:47 +0900

    [FIX] header jquery css 수정

--- js/main.js
+++ js/main.js
@@ -1,14 +1,17 @@
+/*성희 main_header 시작*/
 $('.main_header_menu > li').click(function() {
-  $('.main_header_menu > li').not(this).find('ul').slideUp(300);
-  $(this).find('ul').stop().slideToggle(300);  
+  $('.main_header_menu > li').not(this).find('ul').slideUp();
+  $(this).find('ul').stop().slideToggle();  
 });
-
 // .main_header_toggleBtn을 클릭하면 .main_header_menu가 보이거나 숨겨집니다.
-$('.main_header_toggleBtn').click(function() {
+$('.main_header_toggleBtn').click(function(e){
+  e.preventDefault();
+  if ($(window).width() < 768) {
   $('.main_header_menu').toggle();
+  }
 });
+/*성희 main_header 종료*/
 
-  $( "#accordion" ).accordion();
 $(function () {
   $(".video_slider").bxSlider({
     controls: false,
