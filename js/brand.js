c65384a45c6c807cfd9a660296f9651e7cfb1353
yurimeeee <137126594+yurimeeee@users.noreply.github.com>
2023-08-08 18:05:05 +0900

    [FEAT] store/notice/brand 코드 수정

--- js/brand.js
+++ js/brand.js
@@ -1,9 +1,9 @@
-/* section 1 - mask */
+/* section 1,2 - mask 효과, 페이드인 */
 let maskTT = $('.sec_1 .mask h2'),
-  sec1 = $('.sec_1_after'),
-  sec2 = $('.sec_2'),
-  sec2OST = $('.sec_2').offset().top,
-  maskActive = true;
+    sec1 = $('.sec_1_after'),
+    sec2 = $('.sec_2'),
+    sec2OST = $('.sec_2').offset().top,
+    maskActive = true;
 
 
 //hide()
@@ -14,195 +14,200 @@ $(window).on('scroll', function () {
   if (sct > 10) {
     //스크롤 발생시, 텍스트 마스크 실행
     maskTT.addClass('active');
-    if (sct > 40) {
+    if (sct > 40){
       setTimeout(() => {
         sec1.addClass('active');
         $('.sec_1_after h2').addClass('animate__fadeInLeft');
         $('.sec_1_after h3').addClass('animate__fadeInRight');
-      }, 500, 'easeInOutBack')
-    }
-
-    // if(f == true){
-    // } else{
-    //   gramInnov.removeClass('animate__fadeInUp');
-    // }
-    // f = false;
-    //한번 실행 후, f 값 변경 
-
+      }, 500, 'easeInOutBack');   
+    // 마스크 실행 후, 텍스트 좌우로 페이드인 
+    };
     if (sct > sec2OST - 400) {
       sec2.find('h2, h3').fadeIn(1000, function () {
         // sec2.find('h3').addClass('active');
-        sec2.find('h3').css({ transition: '0.5s', transform: 'scale(1.5)' });
-      })
-      //sec2 페이드인
-    }
+        sec2.find('h3').css({ transition: '0.5s', transform: 'scale(1.5)'});
+      });
+      // sec2 텍스트 페이드인
+    };
   };
 });
 
-
-/* minus effect */
+/* section 3 - minus effect, 페이드인, 노트북 애니메이션 */
 let minusIcon = $('.minus'),
     minusImg = $('.minus_effect img');
 
-$(window).on('scroll', function ()  {
+$(window).on('scroll', function () {
   let sct = $(window).scrollTop(), //스크롤 양 확인
-      count = 0,                  //이미지 index count 
+      count = 0,                   //이미지 index count 
       minusOST = $('.sec_3').offset().top,
       kgGram = $('.kg_gram');
 
   if (sct > minusOST) {
     minusIcon.addClass('acitve');
     $('.minus_bg').fadeOut(1000);
-  };
-  if (sct > minusOST +100 ) {
+  }; 
+  // minus_bg 페이드 아웃
+  if (sct > minusOST + 100) {
     setTimeout(() => {
       kgGram.addClass('active animate__fadeIn');
-    }, 1000, 'easeInOutBack')
+    }, 1000, 'easeInOutBack');
   };
-  
-  //console.log($('.sec_4').innerHeight());
- /* 그램 notebook 애니메이션 */
+
+  // 그램 노트북 애니메이션, 스크롤 양에 따라 count 변수로 이미지 인덱스 변경
   let minusAniOST = $('.minus_effect').innerHeight();
   count = Math.floor(((sct) / minusAniOST) * 60)
-  if (count > 1) { 
-      //console.log('카운트', count);
-    minusImg.attr({ "src": `imgs/brand/brand_minus/minus_${count}.png`}) 
+  if (count > 1 && count <= 110) {
+    //console.log('카운트', count);
+    minusImg.attr({ "src": `imgs/brand/brand_minus/minus_${count}.png` })
   }
 
- /* 그램 weight 애니메이션 */
+  // weight 텍스트 페이드인
   let minusAfter = $('.minus_after'),
-      minuseffectOST = $('.minus_effect').offset().top,
-      weightTT = $('.weight li'),
-      weightUpCount = weightTT.length,
-      currentIdx = 0;
-  
-    let weightStop;
-    let isActive = false;
+    minuseffectOST = $('.minus_effect').offset().top,
+    weightTT = $('.weight li'),
+    weightUpCount = weightTT.length,
+    currentIdx = 0;
 
-    function weightUp(){
-      
-      let nextIdx = (currentIdx + 1) % weightUpCount;
-  
-      weightTT.eq(currentIdx).css({ transformOrigin: '50% 0'});
-      weightTT.eq(currentIdx).addClass('fold');
+  let weightStop;
+  let isActive = false;
 
-      // weightTT.eq(currentIdx).stop().animate({ transform: 'scaleY(0)' }, 1000, "linear");
+  function weightUp() {
 
-      weightTT.eq(nextIdx).css({ transformOrigin: '50% 100%' });
-      weightTT.eq(nextIdx).addClass('spread');
+    let nextIdx = (currentIdx + 1) % weightUpCount;
 
-      setTimeout(()=>{
-        weightTT.eq(currentIdx).removeClass('fold');
-        weightTT.eq(nextIdx).removeClass('spread');
-      })
-    
-      // weightTT.eq(nextIdx).stop().animate({ transform: 'scaleY(1)' }, 1000,"linear");
-      currentIdx = nextIdx;
-      if (currentIdx == 2) {
-        clearInterval(weightStop);
-     
-      }console.log(currentIdx)
-    }   
-    
+    weightTT.eq(currentIdx).css({ transformOrigin: '50% 0' });
+    weightTT.eq(currentIdx).addClass('fold');
+
+    // weightTT.eq(currentIdx).stop().animate({ transform: 'scaleY(0)' }, 1000, "linear");
+
+    weightTT.eq(nextIdx).css({ transformOrigin: '50% 100%' });
+    weightTT.eq(nextIdx).addClass('spread');
+
+    setTimeout(() => {
+      weightTT.eq(currentIdx).removeClass('fold');
+      weightTT.eq(nextIdx).removeClass('spread');
+    })
+
+    // weightTT.eq(nextIdx).stop().animate({ transform: 'scaleY(1)' }, 1000,"linear");
+    currentIdx = nextIdx;
+    if (currentIdx == 2) {
+      clearInterval(weightStop);
+
+    };
+  }
+
   if (sct > minuseffectOST) {
-    if(!isActive){
+    if (!isActive) {
       weightStop = setInterval(() => {
         weightUp();
-       
-       }, 3000);
-       isActive = true;
-       console.log(isActive)
+
+      }, 3000);
+      isActive = true;
+      //console.log(isActive)
     }
-   
 
-
-    //  let weightUp = setInterval(() => {
-    //    let nextIdx = (currentIdx + 1) % weightUpCount;
-    //    weightTT.eq(currentIdx).show().css({ transformOrigin: '50% 0' }).animate({ transform: 'scaleY(0)', opacity: 1 }, 1000);
- 
-    //    weightTT.eq(nextIdx).css({ transformOrigin: '50% 100%' }).animate({ transform: 'scaleY(1)' }, 1000);
-    //    currentIdx = nextIdx;
- 
-    //    //console.log(currentIdx)
-    //    if (currentIdx == 2) {
-    //      clearInterval(weightUp);
-    //    }
-    //  }, 3000);
 
     let plusIcon = $('.plus'),
-        plusOSP = $('.sec_4').offset().top,
-        divisionIcon = $('.division'),
-        divisionOSP = $('.sec_5').offset().top,
-        multipleIcon = $('.multiple'),
-        multipleOSP = $('.sec_6').offset().top,
-        twentyOST = $('.twenty').offset().top;
-        count = 1; 
-        
+      plusOSP = $('.sec_4').offset().top,
+      divisionIcon = $('.division'),
+      divisionOSP = $('.sec_5').offset().top,
+      multipleIcon = $('.multiple'),
+      multipleOSP = $('.sec_6').offset().top,
+      twentyOST = $('.twenty').offset().top;
+      count = 1;
+      let batterySwitch = false;
 
 
+
+      // function batteryAnimation() {
+      //   let sct = $(window).scrollTop(),
+      //   count = Math.floor(((sct) / divisionOSP) * 3)
+      //   const battery = $('.division_tt img')
+      //   // count++;
+      //   battery.attr({ "src": `imgs/brand/brand_division/battery/battery_${count}.png` })
+      // }
+      
     //console.log(plusOSP);
+    console.log( $('.division_tt img').innerHeight());
     if (sct > plusOSP) {
       plusIcon.addClass('acitve');
       $('.plus_bg').fadeOut(1000);
+      
     }
     if (sct > divisionOSP) {
       divisionIcon.addClass('acitve');
       $('.division_bg').fadeOut(1000);
-      batteryAnimation();
-      // count = Math.floor(((sct) / divisionOSP) * 3)
-      // if (count > 1) { 
-      //     console.log('디비젼카운트', count);
+      if(!batterySwitch){
+        let i = 1;
+        let battery = setInterval(() => {
+          $('.division_tt img').attr({ "src": `imgs/brand/brand_division/battery/battery_${i}.png`});
+          i++;
+          if(i == 25){
+            clearInterval(battery);
+          }
+        }, 100);
+        batterySwitch = true;
+      };
+     //batteryAnimation();
+      // count = Math.floor(((sct) / divisionOSP) * 8)
+      // count = Math.floor(((sct) / $('.division_tt img').innerHeight()/ 6))
+      // if (count > 1 && count <=23) { 
       //   $('.division_tt img').attr({ "src": `imgs/brand/brand_division/battery/battery_${count}.png`}) 
       // }
-    }
-    if (sct > divisionOSP+10) {
-      batteryAnimation();
-    }
+      // if (count = 23) { 
+      // $('.division_tt img').attr({ "src": `imgs/brand/brand_division/battery/battery_24.png`}) 
+      // }
+    //   let i = 1;
+    //   function batteryAnimation(){
+    //     setInterval((i)=>{
+    //       if(i != 24){
+       
+    //         $('.division_tt img').attr({ "src": `imgs/brand/brand_division/battery/battery_${i}.png`}) 
+    //         i++;
+    //       }
+    //     },100)
+    //   }
+    //   batteryAnimation();
       
-    
+    // }
+
     if (sct > multipleOSP) {
       multipleIcon.addClass('acitve');
       $('.multiple_bg').fadeOut(1000);
-   };
-   if(sct > multipleOSP + 30){
-    // $('.vivid').addClass('active');
-    $('.vivid').addClass('active animate__fadeInUp');
-  }
-  if(sct > multipleOSP + 80){
-    $('.multiple_tt').find('h2').eq(0).addClass('animate');
-    setTimeout(()=>{
-      $('.multiple_tt').find('h2').eq(1).addClass('animate');
-    },1000)
-  }
-  if(sct > twentyOST ){
-    $('.twenty h2').addClass('active animate__fadeIn');
-  }
-
-}});
-
-function batteryAnimation() {
-  const battery = $('.division_tt img')
-  count++;
-  battery.attr({ "src": `imgs/brand/brand_division/battery/battery_${count}.png`})
-}
+    };
+    if (sct > multipleOSP + 30) {
+      // $('.vivid').addClass('active');
+      $('.vivid').addClass('active animate__fadeInUp');
+    }
+    if (sct > multipleOSP + 80) {
+      $('.multiple_tt').find('h2').eq(0).addClass('animate');
+      setTimeout(() => {
+        $('.multiple_tt').find('h2').eq(1).addClass('animate');
+      }, 1000)
+    }
+    if (sct > twentyOST) {
+      $('.twenty h2').addClass('active animate__fadeIn');
+    }
+ }
+};
 
 
 
 
 
-$('.plus_tt').hide();
-$(window).on('scroll',function () {
+// $('.plus_tt').hide();
+$(window).on('scroll', function () {
   let sct = $(window).scrollTop(),
-      plusOpenOST = $('.plus_open').offset().top,
-      count = Math.floor(((sct) / plusOpenOST) * 20);
+    plusOpenOST = $('.plus_open').offset().top,
+    count = Math.floor(((sct) / plusOpenOST) * 20);
   console.log('sct :', sct);
-  if (count >1 && count < 64) {
+  if (count > 1 && count < 64) {
     $('.plus_effect img').attr({ "src": `imgs/brand/brand_plus/plus_${count}.png` });
     if (count == 20) {
-      $('.plus_tt h2').addClass('active');
-      $('.plus_tt h2').addClass('animate__fadeIn');
+      // $('.plus_tt h2').addClass('active');
+      $('.plus_tt h2').addClass('active animate__fadeIn');
     }
-    
+
   }
 })
 
@@ -210,69 +215,78 @@ $(window).on('scroll',function () {
 let plusImg = $('.plus_open img');
 
 
-$(window).scroll(function () {
-  let sct = $(window).scrollTop();
-  let plusOSP = $('.sec_4').offset().top;
-  if (sct > plusOSP - 300) {
-    $('.plus_bg').fadeOut(1000);
-    
-  }
-})
+// $(window).scroll(function () {
+//   let sct = $(window).scrollTop();
+//   let plusOSP = $('.sec_4').offset().top;
+//   if (sct > plusOSP - 300) {
+//     $('.plus_bg').fadeOut(1000);
+
+//   }
+// })
 
 
 
-// $ ( '.20twenty-container'). twentytwenty ({ default_offset_pct : 0.3 });
-
+/* twentytwenty / gram 해상도 비교  */
 $(".20twenty-container").twentytwenty({
-  default_offset_pct: 0.4, // How much of the before image is visible when the page loads
-  // no_overlay: true, //Do not show the overlay with before and after
-  move_slider_on_hover: true, // Move slider on mouse hover?
-  move_with_handle_only: true, // Allow a user to swipe anywhere on the image to control slider movement. 
-  click_to_move: true // Allow a user to click (or tap) anywhere on the image to move the slider to that location.
+  default_offset_pct: 0.4,
+  move_slider_on_hover: true,
+  move_with_handle_only: true,
+  click_to_move: true
 });
 
 
 
 let divisionIcon = $('.division'),
-    multipleIcon = $('.multiple');
-    //minusImg = $('.minus_effect img');
+  multipleIcon = $('.multiple');
+//minusImg = $('.minus_effect img');
 
-$(window).on('scroll', function ()  {
+$(window).on('scroll', function () {
   let sct = $(window).scrollTop(); //스크롤 양 확인
   //let count = 0;                   //이미지 index count 
   let divisionOST = $('.sec_5').offset().top;
   let multiplenOST = $('.sec_6').offset().top;
-  
+
   if (sct > divisionOST - 400) {
     // divisionIcon.addClass('acitve');
     // $('.division_bg').fadeOut(1000);
     if (sct > divisionOST) {
-      setTimeout(() => {
-        $('.division_tt').addClass('active');
-        $('.division_tt').addClass('animate__fadeIn');
-      }, 800, 'easeInOutBack');
-      let count = 0; 
+      let divisionTT = $('.division_tt');
+      divisionTT.find('.d_time').addClass('active animate__fadeIn');
+      setTimeout(()=>{
+        divisionTT.find('.d_day').addClass('active animate__fadeIn');
+        setTimeout(()=>{
+          divisionTT.find('.d_long').addClass('active animate__fadeIn');
+        },1000)
+      },1000)
+      
+  
+      // divisionTT.find('.d_time').addClass('active animate__fadeIn');
+      // setTimeout(() => {
+      //   $('.division_tt').addClass('active');
+      //   $('.division_tt').addClass('animate__fadeIn');
+      // }, 800, 'easeInOutBack');
+      let count = 0;
 
       plusOpenOST = $('.plus_open').offset().top,
-      count = Math.floor(((sct) / plusOpenOST) * 20);
-  console.log('sct :', sct);
-  if (count >1 && count < 64) {
-    $('.plus_effect img').attr({ "src": `imgs/brand/brand_plus/plus_${count}.png` });
+        count = Math.floor(((sct) / plusOpenOST) * 20);
+      console.log('sct :', sct);
+      if (count > 1 && count < 64) {
+        $('.plus_effect img').attr({ "src": `imgs/brand/brand_plus/plus_${count}.png` });
 
+      }
     }
-  }
 
 
 
-  let dolbyAniOST = $('.dolby_wrap img').offset().top;
-  //console.log('돌비OST', dolbyAniOST);
-  count = Math.floor(((sct) / dolbyAniOST) * 50)
-  if (count > 1) { 
+    let dolbyAniOST = $('.dolby_wrap img').offset().top;
+    //console.log('돌비OST', dolbyAniOST);
+    count = Math.floor(((sct) / dolbyAniOST) * 50)
+    if (count > 1) {
       //console.log('돌비카운트', count);
 
-    $('.dolby_wrap img').attr({ "src": `imgs/brand/dolby/dolby_${count}.png`}) 
+      $('.dolby_wrap img').attr({ "src": `imgs/brand/dolby/dolby_${count}.png` })
+    }
   }
-}
 });
 
 
@@ -284,27 +298,29 @@ $(window).on('scroll', function ()  {
 let sec8 = $('.sec_8');
 let switch1 = false;
 
-$(window).on('scroll',function(){
+$(window).on('scroll', function () {
 
   let sec8OST = sec8.offset().top;
   let sct = $(window).scrollTop();
 
   console.log(sec8OST)
-  if(!switch1){
-    if(sct > sec8OST){
+  if (!switch1) {
+    if (sct > sec8OST) {
       sec8.find('.sec_8_tt1').fadeIn();
-      
+
       if (sct > sec8OST + 30) {
         sec8.find('.sec_8_tt1').fadeOut();
         sec8.find('.sec_8_tt2').fadeIn();
-  
+
         // if (sct > sec8OST + 0) {
         //   sec8.find('.sec_8_tt1').fadeout();
         // }
       }
       //sec8.find('.sec_8_tt1').addClass('active');
     }
-  
+
   }
   switch1 = false;
-})
+  })
+});
+
