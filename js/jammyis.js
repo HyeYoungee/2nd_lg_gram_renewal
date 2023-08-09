5f501d111a66a8fc5dada284dedee9c81b3cb16b
HyeYoungee <137126899+HyeYoungee@users.noreply.github.com>
2023-08-10 08:32:57 +0900

    [ADD] jammy_is page 반응형 css 코드 추가

--- js/jammyis.js
+++ js/jammyis.js
@@ -1,15 +1,13 @@
-let scrollTop = $(window).scrollTop();
-let sec02_content = $('.jammy_is_sec02 > div > div');
+// let scrollTop = $(window).scrollTop();
+// let sec02_content = $('.jammy_is_sec02 > div > div');
 
-console.log(sec02_content)
+// console.log(sec02_content)
 
-sec02_content.each(function(){
-  
+// sec02_content.each(function(){
+//   $(window).scroll(function(){
+//   });
+// })
 
-  $(window).scroll(function(){
-
-  
-  
-  });
-  
-})
+/* jammy_is_sec04 */
+let jammyIs_sec04 = $('.jammy_is_sec04');
+let jamFill = jammyIs_sec04.find('')
