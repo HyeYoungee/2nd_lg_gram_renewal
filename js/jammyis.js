0e632f11efa133bd27bcece3b9e1224ed96be077
HyeYoungee <137126899+HyeYoungee@users.noreply.github.com>
2023-08-09 16:47:51 +0900

    [ADD] 코드 수정 및 추가

--- js/jammyis.js
+++ js/jammyis.js
@@ -1,1 +1,15 @@
-// let scrollTop = $(window).scrollTop(),
+let scrollTop = $(window).scrollTop();
+let sec02_content = $('.jammy_is_sec02 > div > div');
+
+console.log(sec02_content)
+
+sec02_content.each(function(){
+  
+
+  $(window).scroll(function(){
+
+  
+  
+  });
+  
+})
