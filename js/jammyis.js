8addc67408501cff493f0840717bd8a0704c9c5d
HyeYoungee <137126899+HyeYoungee@users.noreply.github.com>
2023-08-10 09:28:12 +0900

    [ADD] jammy_is page 코드 추가

--- js/jammyis.js
+++ js/jammyis.js
@@ -1,13 +1,4 @@
-// let scrollTop = $(window).scrollTop();
-// let sec02_content = $('.jammy_is_sec02 > div > div');
+let off = $(window).scrollTop();
+console.log(off);
 
-// console.log(sec02_content)
-
-// sec02_content.each(function(){
-//   $(window).scroll(function(){
-//   });
-// })
-
-/* jammy_is_sec04 */
-let jammyIs_sec04 = $('.jammy_is_sec04');
-let jamFill = jammyIs_sec04.find('')
+AOS.init();
